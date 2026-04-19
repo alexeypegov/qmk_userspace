ab5ff1fe7db8c039acc6bdd6230e2c9711d2f613
Alexey Pegov <apegov@animatron.com>
2026-04-19 14:29:45 +0200

    add config.h & rules.mk

---
+++ keyboards/cantor/keymaps/alexeypegov/config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define TAPPING_TERM 200
+#define TAPPING_TERM_PER_KEY
+#define TAPPING_TERM_THUMB 125
+
+#define HOLD_ON_OTHER_KEY_PRESS
+#define PERMISSIVE_HOLD
+
+#define COMBO_COUNT 3
+#define COMBO_TERM 40
+
+#define ONESHOT_TAP_TOGGLE 3
+#define ONESHOT_TIMEOUT 2000
+
+#define MASTER_LEFT
